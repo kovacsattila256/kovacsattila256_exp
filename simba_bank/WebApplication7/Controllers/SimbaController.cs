b299ba1bb5dc49bf0c2554659b8459230e91d3c2
kovacsattila256 <kovacs.attila.ab@gmail.com>
2018-07-04 11:43:23 +0200

    new

---
+++ simba_bank/WebApplication7/Controllers/SimbaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class BankAccountController : Controller
+    {
+        [Route("simba")]
+        public IActionResult Simba()
+        {
+            BankAccount account = new BankAccount() { Name = "Simba", Balance = 2000, AnimalType = "Lion" };
+            return View(account);
+        }
+
+        [Route("accounts")]
+        public IActionResult Accounts()
+        {
+            List<BankAccount> accounts = new List<BankAccount>(){
+            new BankAccount(){Id= 1, Name = "Toki", Balance = 3000, AnimalType = "dawg" },
+            new BankAccount(){Id= 2, Name = "Negro", Balance = 1400, AnimalType = "cock" },
+            new BankAccount(){Id= 3, Name = "Cuncus", Balance = 720, AnimalType = "whale" },
+            new BankAccount(){Id= 4, Name = "Killer", Balance = 10, AnimalType = "rabbit" }
+            };
+
+            return View(accounts);
+        }
+    }
+}
---
+++ simba_bank/WebApplication7/Models/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class BankAccount
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public string AnimalType { get; set; }
+        public string Currency { get; set; }
+
+        public BankAccount()
+        {
+            Currency = "Zebra";
+        }
+    }
+}
